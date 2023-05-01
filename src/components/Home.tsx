bc0f695a270d72221fbedb70f5bcf8f1bcb07653
tomashrib <tomas.hrib@proton.me>
2023-05-01 18:23:39 +0200

    fix: on iphone safari tel-number decorations are weird

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <meta name="format-detection" content="telephone=no" />
       </Head>
 
       <Layout>
