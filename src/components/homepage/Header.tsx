efb1250dd57d7af43f34d4ea41a1c27a033a1c8c
tomashrib <tomas.hrib@proton.me>
2023-05-03 08:00:23 +0200

    feat: add draft-2 header (mobile-view)

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -2,26 +2,30 @@ import styled from "styled-components";
 
 const HeaderWrapper = styled.header`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  padding: 0.5rem 0;
+  justify-content: space-between;
   background-color: #0D101B;
   width: 100%;
   color: #fff;
   gap: 1rem;
+  padding: 3rem 1rem 3rem 1rem;
 
-  .title {
+  .title-logo {
     font-size: 0.8rem;
     align-self: flex-start;
-    margin-left: 1rem;
   }
 `;
 
 const Header = () => {
   return (
     <HeaderWrapper>
-      <div className="title">DimensionLab</div>
+      <div className="title-logo">
+        <img src="assets/branding/header-logo-dl.svg" alt="" />
+      </div>
+      <div className="hamburger-menu">
+        <img src="assets/component-assets/hamburger-menu-header.svg" alt="" />
+      </div>
     </HeaderWrapper>
   );
 };
