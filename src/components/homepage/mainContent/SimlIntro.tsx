a8b5d8fdfeabb4c9b7c23038d3e844ca13a0ada7
tomashrib <tomas.hrib@proton.me>
2023-05-03 10:00:59 +0200

    feat: add draft-2 siml-intro (mobile-view)

--- src/components/homepage/mainContent/SimlIntro.tsx
+++ src/components/homepage/mainContent/SimlIntro.tsx
@@ -3,8 +3,7 @@ import styled from "styled-components";
 const SimlIntroWrapper = styled.section`
   display: grid;
   width: 100%;
-  padding-left: 3rem;
-  padding-right: 3rem;
+  padding: 2rem 1rem 2rem 1rem;
   grid-auto-flow: row;
   align-items: center;
   justify-items: start;
@@ -13,7 +12,22 @@ const SimlIntroWrapper = styled.section`
   .logos{
     display: flex;
     width: 100%;
-    column-gap: 5rem;
+    flex-direction: row;
+    justify-items: flex-start;
+    gap: 1.2rem;
+
+    .products-vertical-text {
+      margin: 0;
+      padding: 0;
+      writing-mode: vertical-lr;
+      transform: rotate(-180deg);
+      color: #61646F;
+    }
+
+    img {
+      margin: 0;
+      padding: 0;
+    }
 
     @media (min-width: 900px) {
       column-gap: 30rem;
@@ -21,16 +35,24 @@ const SimlIntroWrapper = styled.section`
   }
 
   .brandName{
-    font-size: 2rem;
-    font-weight: 700;
-    color: #fff;
+    padding-left: 2.5rem;
+    img {
+      width: 12rem;
+    }
   }
 
   .heading{
+    padding-left: 2.5rem;
     font-size: 1.4rem;
     font-weight: 700;
     color: #fff;
     text-align: start;
+  }
+
+  a {
+    padding-left: 2.5rem;
+    .view-more {
+    }
   }
 `;
 
@@ -38,11 +60,18 @@ const SimlIntro = () => {
   return (
     <SimlIntroWrapper>
       <div className="logos">
+        <div className="products-vertical-text">PRODUCTS</div>
         <img src="/assets/siml_outline_logo.svg" alt="siml logo" />
-        <img src="/assets/siml_S_outline_logo.svg" alt="siml logo" />
       </div>
-      <div className="brandName">SIML.AI</div>
-      <div className="heading">Software platform for working with high-performance AI-based numerical simulators.</div>
+      <div className="brandName">
+        <img src="assets/branding/simlai-title-siml-intro.svg" alt="siml ai name title" />
+      </div>
+      <div className="heading">Software platform <br/> for high-performance AI-based numerical simulators.</div>
+      <div className="view-more">
+        <a href="">
+          <img src="assets/component-assets/button-view-more-siml-intro.svg" alt="" />
+        </a>
+      </div>
     </SimlIntroWrapper>
   );
 };
