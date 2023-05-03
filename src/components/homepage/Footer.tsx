2a4c3e071e7c2eac8e5791aa8a7f1391c0387506
tomashrib <tomas.hrib@proton.me>
2023-05-03 11:19:37 +0200

    feat: add draft-2 footer (mobile-view)

--- src/components/homepage/Footer.tsx
+++ src/components/homepage/Footer.tsx
@@ -5,207 +5,191 @@ const FooterWrapper = styled.footer`
   grid-template-columns: repeat(1, 1fr);
   justify-items: center;
   align-items: center;
-  padding-top: 5rem;
-  padding-bottom: 5rem;
-  padding-left: 3rem;
-  padding-right: 3rem;
-  background-color: #010012;
+  padding: 3rem 1rem 3rem 1rem;
+  background-color: #222530;
   width: 100%;
   color: #fff;
-  gap: 2.5rem;
+  gap: 2rem;
 
-  @media (min-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-    justify-items: center;
-  }
-
-  @media (min-width: 1200px) {
-    grid-template-columns: repeat(4, 1fr);
-    justify-items: end;
-    align-items: start;
-    padding-top: 10rem;
-    padding-bottom: 10rem;
-  }
-
-  .logo {
-    display: flex;
-
-    @media (min-width: 768px) {
-      width: 100%;
-      align-self: start;
-    }
-
-    @media (min-width: 1200px) {
-      padding-left: 10rem;
-    }
-
-    img {
-      width: 4rem;
-
-      @media (min-width: 768px) {
-        width: 4rem;
-      }
-
-      @media (min-width: 1200px) {
-        width: 7rem;
-      }
-    }
-  }
-  .products {
+  .content-container{
     display: flex;
     flex-direction: column;
-    align-items: center;
-    gap: 0.5rem;
+    padding-left: 2.5rem;
+    width: 100%;
+    gap: 2rem;
 
-    @media (min-width: 768px) {
+    .dl-title-image{
       width: 100%;
-      align-self: start;
-    }
 
-    .title {
-      font-size: 1.4rem;
-      font-weight: 700;
-
-      @media (min-width: 768px) {
-        align-self: start;
-      }
-    }
-    .simlai-redirect {
-      font-size: 1rem;
-      color: white;
-      text-decoration: none;
-      border-bottom: 2px solid #2460FF;
-      transition: all 0.3s ease-in-out;
-
-      @media (min-width: 768px) {
-        align-self: start;
-      }
-
-      &:hover {
-        transform: rotate(360deg);
-      }
-    }
-  }
-  .useful-links {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 0.5rem;
-
-    @media (min-width: 768px) {
-      width: 100%;
-      align-self: start;
-    }
-
-    .title {
-      font-size: 1.4rem;
-      font-weight: 700;
-
-      @media (min-width: 768px) {
-        align-self: start;
+      img{
+        width: 13rem;
       }
     }
 
-    a {
-      font-size: 1rem;
-      color: white;
-      text-decoration: none;
-      border-bottom: 2px solid #2460FF;
-      transition: all 0.3s ease-in-out;
-
-      @media (min-width: 768px) {
-        align-self: start;
-      }
-
-      &:hover {
-        transform: rotate(360deg);
-      }
-    }
-  }
-  .contact-us {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 0.5rem;
-
-    @media (min-width: 768px) {
-      align-items: start;
-      width: 100%;
-    }
-
-    .title {
-      font-size: 1.4rem;
-      font-weight: 700;
-    }
-
-    .location {
+    .socials{
       display: flex;
-      gap: 0.5rem;
-      align-items: center;
-      transition: all 0.3s ease-in-out;
+      flex-direction: row;
+      gap: 1.5rem;
+    }
 
-      &:hover {
-        transform: scale(1.1);
+    .products {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      .title{
+        color: #61646F;
+        font-size: 1rem;
+      }
+
+      .simlai {
+        a {
+          text-decoration: none;
+          color: white;
+          font-size: 1.1rem;
+        }
       }
     }
-    .tel-contact {
+    
+    .explore {
       display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      .title{
+        color: #61646F;
+        font-size: 1rem;
+      }
+      .links {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+
+        a{
+          text-decoration: none;
+          color: white;
+          font-size: 1.1rem;
+        }
+        .tos{}
+        .privacy-policy{}
+      }
+    }
+
+    .contact-us {
+      display: flex;
+      flex-direction: column;
+      font-size: 0.8rem;
       gap: 0.5rem;
-      align-items: center;
-      transition: all 0.3s ease-in-out;
+      padding-bottom: 2rem;
+
+      img {
+        width: 0.6rem;
+      }
       
-      .tel-number {
-        text-decoration: none;
+      .title {
+        font-size: 1rem;
+        color: #61646F;
+        padding-bottom: 0.5rem;
       }
 
-      &:hover {
-        transform: scale(1.1);
+      .location{
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
       }
-    }
-    .email-contact {
-      display: flex;
-      gap: 0.5rem;
-      align-items: center;
-      transition: all 0.3s ease-in-out;
-
-      &:hover {
-        transform: scale(1.1);
+      .phone{
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+      }
+      .email{
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
       }
     }
   }
+  .copyright {
+    color: gray;
+    display: flex;
+    justify-self: flex-start;
+    font-size: 0.8rem;
+  }
+}
 `;
 
 const Footer = () => {
   return (
     <FooterWrapper>
-      <div className='logo'>
-        <img src="assets/logo_D.svg" alt="" />
-      </div>
-      <div className="products">
-        <div className='title'>PRODUCTS</div>
-        <a href="https://siml.ai" className='simlai-redirect'>SIML.ai</a>
-      </div>
-      <div className="useful-links">
-        <div className='title'>USEFUL LINKS</div>
-        <a href="">Contact</a>
-        <a href="">Terms & Conditions</a>
-        <a href="">Privacy Policy</a>
-      </div>
-      <div className="contact-us">
-        <div className='title'>CONTACT US</div>
+      <div className="content-container">
+        <div className="dl-title-image">
+          <img src="assets/branding/dl-title-intro.svg" alt=""/>
+        </div>
+        <div className="socials">
+          <div className="linkedin">
+            <a href="https://www.linkedin.com/company/dimensionlab">
+              <img src="assets/component-assets/footer/linkedin-logo.svg" alt="" />
+            </a>
+          </div>
+          <div className="facebook">
+            <a href="https://www.facebook.com/dimensionlab/">
+              <img src="assets/component-assets/footer/facebook-logo.svg" alt="" />
+            </a>
+          </div>
+          <div className="twitter">
+            <a href="https://twitter.com/TheDimensionLab">
+              <img src="assets/component-assets/footer/twitter-logo.svg" alt="" />
+            </a>
+          </div>
+          <div className="instagram">
+            <a href="">
+              <img src="assets/component-assets/footer/instagram-logo.svg" alt="" />
+            </a>
+          </div>
+          <div className="medium">
+            <a href="">
+              <img src="assets/component-assets/footer/medium-logo.svg" alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="products">
+          <div className="title">PRODUCTS</div>
+          <div className="simlai">
+            <a href="https://siml.ai">SIML.ai</a>
+          </div>
+        </div>
+        <div className="explore">
+          <div className="title">EXPLORE</div>
+          <div className="links">
+            <div className="tos">
+              <a href="">
+                Terms & Conditions
+              </a>
+            </div>
+            <div className="privacy-policy">
+              <a href="">
+                Privacy Policy
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="contact-us">
+          <div className="title">CONTACT US</div>
           <div className="location">
-            <span className="material-symbols-outlined">location_on</span>
-            <div className="address">Lomnická 2, 040 01 Košice, Slovakia</div>
+            <img src="assets/component-assets/footer/location-icon.svg" alt="" />
+            <div>Lomnická 2, 040 01 Košice, Slovakia</div>
           </div>
-          <div className="tel-contact">
-          <span className="material-symbols-outlined">call</span>
-            <div className="tel-number">+421 911 334 797</div>
+          <div className="phone">
+            <img src="assets/component-assets/footer/phone-icon.svg" alt="" />
+            <div>+421 911 334 797</div>
           </div>
-          <div className="email-contact">
-          <span className="material-symbols-outlined">mail</span>
-            <div className="email">hello@dimensiolab.org</div>
+          <div className="email">
+            <img src="assets/component-assets/footer/email-icon.svg" alt="" />
+            <div>hello@dimensionlab.org</div>
           </div>
+        </div>
       </div>
+      <div className="copyright">© 2023 DimensionLab s.r.o.</div>
     </FooterWrapper>
   );
 };
